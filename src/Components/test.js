9c8532a12343619d0b5fa1ef6558a2f6d75d8b71
Ankan <ankanbhowmick04@gmail.com>
2023-07-14 11:16:03 +0530

    first commit

---
+++ src/Components/test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function test() {
+  return (
+    <div>
+      Hello
+      <img src="./Images/model-s.jpg" alt="" />
+    </div>
+  )
+}
+
+export default test
---
+++ src/Tooth.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Tooth() {
+  return (
+    <div>
+            <img src="./Images/model-s.jpg" alt="" />
+
+    </div>
+  )
+}
+
+export default Tooth
